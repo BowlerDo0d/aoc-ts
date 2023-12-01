c4a9e080657cf8bc03a62b37051611f4dbc93002
BowlerDo0d <smahony39@gmail.com>
2023-12-01 10:30:46 -0500

    updated starting file

--- template/ts/index.ts
+++ template/ts/index.ts
@@ -1,12 +1,17 @@
 import getInput from '../../../utils/getInput';
 
-const part1 = () => {
+const pathParts = __dirname.split('/'),
+  day = pathParts?.length ? pathParts[pathParts.length-1] : '',
+  year = pathParts?.length ? pathParts[pathParts.length-2] : '',
+  fullInput = getInput(year, day);
+
+const part1 = (input: string) => {
 
 }
 
-const part2 = () => {
-    
+const part2 = (input: string) => {
+
 }
 
-console.log(`Solution 1: ${part1()}`);
-console.log(`Solution 2: ${part2()}`);
+console.log(`Solution 1: ${part1(fullInput)}`);
+console.log(`Solution 2: ${part2(fullInput)}`);
